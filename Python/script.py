b24966dfeb0777b4febc1584553e16eaf358e8ef
VincenzoPetrone <51163843+v8p1197@users.noreply.github.com>
2020-06-19 18:01:36 +0200

    first commit
    project setup
    
    Signed-off-by: VincenzoPetrone <v.petrone6@studenti.unisa.it>

---
+++ Python/script.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello SDA@py")
+
+
+if __name__ == '__main__':
+    main()
---
+++ R/script.R
@@ -0,0 +1,7 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: v9pet
+# Created on: 19/06/2020
+
+print("Hello SDA@R")
+plot(1:20, pch=1:20, col=sample(255, 20, replace=T))
