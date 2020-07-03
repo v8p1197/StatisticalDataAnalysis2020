e2301b9b103cd7c3a9705d6f07ec47d20cd27e4e
VincenzoPetrone <v.petrone6@studenti.unisa.it>
2020-07-03 18:52:15 +0200

    added library functions into preprocessing.R

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -4,6 +4,34 @@ library(readr)
 library(anchors)
 library(mice)
 
+## Utility functions
+#'
+#' Removes NA from a feature
+#'
+#' Takes a feature of a dataset and removes every NA value, returning only the the unique feature values
+#' @param feature the feature to remove NA values from
+#' @return the non-NA unique values of the input feature
+#' @usage na.omit.unique(feature)
+#' @export
+na.omit.unique <- function(feature) {
+  return (unique(na.omit(feature)))
+}
+
+#'
+#' Renames a column of a dataset
+#' 
+#' Takes a dataset and a name, and renames the column with that name with a new one, returning the dataset itself
+#' @param data the dataset containing the column to rename
+#' @param name.old the old name for the column to rename
+#' @param name.new the new name for the column
+#' @usage rename(data, name.old, name.new)
+#' @export
+#'
+rename <- function(data, name.old, name.new) {
+  names(data)[names(data) == name.old] <- name.new
+  return (data)
+}
+
 ## Data Loading
 # Load dataset 1
 data <- read_excel("Data/dataset_airline.xlsx")
